8bfaa1f793fe2525333b9a9d65144d68e6775510
Wiley Evans <thinknoise@users.noreply.github.com>
2025-05-06 21:30:28 -0700

    recording band, drag onto track, playback

--- src/components/BankSamplelist.js
+++ src/components/BankSamplelist.js
@@ -1,46 +1,33 @@
 import React, { useEffect, useState, useCallback } from "react";
 import BankSample from "./BankSample";
-import { fetchAudioData } from "../utils/fetchAudioData";
+import RecordingBankList from "./BankRecordingList";
+import { fetchAudioData as fetchAudio } from "../utils/fetchAudioData";
 import banks from "../data/banks.json";
-import { useRecorder } from "../hooks/useRecorder";
 import { getAudioContext } from "../utils/audioManager";
-import { BiSolidMicrophoneAlt } from "react-icons/bi";
-import { PiMicrophoneSlashDuotone } from "react-icons/pi";
 
 import "../style/bankTab.css";
 
+/**
+ * BankSampleList component
+ * Renders bank samples or the recording list based on the selected tab
+ */
 const BankSampleList = ({ handleDragStart }) => {
   const [buttons, setButtons] = useState([]);
-  const [bankFilename, setBankFilename] = useState("onehits.json"); // Default bank - no space . json
-  const [recordedBuffer, setRecordedBuffer] = useState(null);
+  const [bankFilename, setBankFilename] = useState("onehits.json");
 
-  const { startRecording, stopRecording, isRecording, audioBuffer } =
-    useRecorder(getAudioContext());
-  // Memoize the spawnButton function
   const spawnButton = useCallback((filename) => {
-    fetchAudioData(filename)
+    fetchAudio(filename)
       .then((data) => {
         if (data) {
           setButtons(data);
-          // console.log('Audio JSON:', data, buttons);
         }
       })
-      .catch((error) => {
-        console.error("Error fetching or setting buttons:", error);
-      });
+      .catch((error) => console.error("Error fetching audio data:", error));
   }, []);
 
-  // Load the initial button data when the component mounts or when bankFilename changes
   useEffect(() => {
     spawnButton(bankFilename);
-  }, [bankFilename, spawnButton]); // Add spawnButton to the dependency array
-
-  useEffect(() => {
-    if (audioBuffer) {
-      setRecordedBuffer(audioBuffer);
-      // console.log("Recorded buffer set!", audioBuffer);
-    }
-  }, [audioBuffer]);
+  }, [bankFilename, spawnButton]);
 
   return (
     <div className="bank-tabs">
@@ -54,58 +41,11 @@ const BankSampleList = ({ handleDragStart }) => {
             ? "Recorded"
             : banks.find((b) => b.filename === filename)?.name || filename}
         </button>
-      ))}{" "}
+      ))}
+
       <div className="button-container">
         {bankFilename === "recorded" ? (
-          <div className="recording-ui">
-            <button
-              className="record-btn-wrapper"
-              style={{
-                zIndex: isRecording ? 0 : 1,
-              }}
-              onClick={startRecording}
-            >
-              <BiSolidMicrophoneAlt
-                size={32}
-                style={{
-                  color: "white",
-                  opacity: isRecording ? 0 : 1,
-                  transition: "opacity 0.2s ease-in-out",
-                }}
-              />
-            </button>
-            <button
-              className="record-btn-wrapper"
-              style={{
-                zIndex: isRecording ? 1 : 0,
-              }}
-              onClick={stopRecording}
-            >
-              <PiMicrophoneSlashDuotone
-                size={32}
-                style={{
-                  color: "red",
-                  opacity: isRecording ? 1 : 0,
-                  transition: "opacity 0.2s ease-in-out",
-                }}
-              />
-            </button>
-            {isRecording && <p>Recording...</p>}
-            {recordedBuffer && (
-              <BankSample
-                key="live-recorded"
-                id="live-recorded"
-                sample={{
-                  id: crypto.randomUUID(),
-                  buffer: recordedBuffer,
-                  filename: "Live Recording",
-                  duration: recordedBuffer.duration,
-                  path: null,
-                  url: null,
-                }}
-              />
-            )}
-          </div>
+          <RecordingBankList handleDragStart={handleDragStart} />
         ) : (
           buttons.map((sample, index) => (
             <BankSample
